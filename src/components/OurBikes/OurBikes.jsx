4df9ba35651ffe2a88c779e3786fb6efb2c699db
m.backi <backimichal@gmail.com>
2023-05-24 16:37:03 +0200

    slider-do poprawki ale lepiej to wygldada

--- src/components/OurBikes/OurBikes.jsx
+++ src/components/OurBikes/OurBikes.jsx
@@ -1,34 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { bikesData } from "../../data/bikesData";
 import styles from "./ourbikes.module.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useTransition, animated } from "react-spring";
 
 const OurBikes = () => {
   const [currentBike, setCurrentBike] = useState(0);
-  const [animationClass, setAnimationClass] = useState("");
   const [selectedDate, setSelectedDate] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [direction, setDirection] = useState("left");
+  const [refreshed, setRefreshed] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (refreshed) {
+      setLoaded(false);
+      setTimeout(() => {
+        setLoaded(true);
+        setRefreshed(false);
+      }, 0);
+    }
+  }, [refreshed]);
 
   const previousBike = () => {
-    setAnimationClass(styles["bikes__presentation--right"]);
-    setTimeout(() => {
-      setAnimationClass("");
-      setCurrentBike((currentBike - 1 + bikesData.length) % bikesData.length);
-    }, 500);
+    setCurrentBike((currentBike - 1 + bikesData.length) % bikesData.length);
+    setDirection("right");
   };
 
   const nextBike = () => {
-    setAnimationClass(styles["bikes__presentation--left"]);
-    setTimeout(() => {
-      setAnimationClass("");
-      setCurrentBike((currentBike + 1) % bikesData.length);
-    }, 500);
+    setCurrentBike((currentBike + 1) % bikesData.length);
+    setDirection("left");
   };
 
   const handleDateSelect = (date) => {
     setSelectedDate(date);
-    // Tu możesz wykonać dowolną logikę związaną z wybraną datą
     console.log("Wybrana data:", date);
+  };
+
+  const transitions = useTransition(currentBike, {
+    config: { duration: 1000 },
+    from: {
+      opacity: 0,
+      transform: `translateX(${direction === "left" ? "-" : ""}100%)`,
+    },
+    enter: { opacity: 1, transform: "translateX(0%)" },
+    leave: {
+      opacity: 0,
+      transform: `translateX(${direction === "left" ? "-" : ""}100%)`,
+    },
+    immediate: !loaded,
+  });
+
+  const handleRefresh = () => {
+    setRefreshed(true);
   };
 
   return (
@@ -41,32 +69,39 @@ const OurBikes = () => {
       </div>
       <div className={styles["bikes__buttons"]}>
         <button
-          className={styles["bikes__buttons--prev"]}
+          className={`${styles["bikes__buttons--prev"]} ${
+            direction === "left" ? styles["left"] : styles["right"]
+          }`}
           onClick={previousBike}
         ></button>
         <button
-          className={styles["bikes__buttons--next"]}
+          className={`${styles["bikes__buttons--next"]} ${
+            direction === "left" ? styles["left"] : styles["right"]
+          }`}
           onClick={nextBike}
         ></button>
 
-        <div className={`${styles["bikes__presentation"]} ${animationClass}`}>
-          <img
-            src={bikesData[currentBike].image}
-            alt="Rower"
-            style={{ width: "90%", height: "280px" }}
-          />
-          <h2>{bikesData[currentBike].name}</h2>
-
-          <div className={styles["bikes__reservation"]}>
-            <p>{bikesData[currentBike].type}</p>
-            <DatePicker
-              selected={selectedDate}
-              minDate={new Date()}
-              onChange={handleDateSelect}
-              placeholderText="Wybierz datę"
+        {transitions((style, item) => (
+          <animated.div className={styles["bikes__presentation"]} style={style}>
+            <img
+              src={bikesData[item].image}
+              alt="Rower"
+              style={{ width: "100%", height: "280px" }}
             />
-          </div>
-        </div>
+            <h2>{bikesData[item].name}</h2>
+
+            <div className={styles["bikes__reservation"]}>
+              <p>{bikesData[item].type}</p>
+              <DatePicker
+                selected={selectedDate}
+                minDate={new Date()}
+                onChange={handleDateSelect}
+                placeholderText="Wybierz datę"
+                wrapperClassName={styles["custom-datepicker-container"]}
+              />
+            </div>
+          </animated.div>
+        ))}
       </div>
     </div>
   );
